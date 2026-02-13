ea47418a5cd5c1da499767bf486d39f4f9d61140
brian <brian@kali.kali>
2026-02-13 07:05:40 -0500

    refactor

--- main.c
+++ main.c
@@ -1,36 +1,52 @@
 #include "raylib.h"
 #include "stdlib.h"
 void DrawImage(Texture2D,Vector2,Rectangle);
+void UpdatePlayer(Rectangle *);
+void CreateWindow(int,int);
+void Draw(Texture2D,Texture2D);
+void RemoveImageFromMem(Texture2D,Texture2D);
 int main() {
-    InitWindow(800, 400, "Scaled & Moving");
-    SetTargetFPS(60);
+  CreateWindow(800,400,"Pear")
     Texture2D background = LoadTexture("assets/game_background.png");
-    Texture2D player = LoadTexture("assets/player.png");
-    Rectangle playerDest = { 100, 100, 64, 64 };
-    Vector2 origin = { 0, 0 };  
-    Rectangle backgroundDest = { 0, 0, 800, 400 };           
+    Texture2D player = LoadTexture("assets/player.png");         
 
     while (!WindowShouldClose()) {
-        Vector2 mousePos = GetMousePosition();
-        playerDest.x = mousePos.x - (playerDest.width / 2);
-playerDest.y = mousePos.y - (playerDest.height / 2);
-
+UpdatePlayer(&playerDest);
         BeginDrawing();
             ClearBackground(BLACK);
-DrawImage(background, origin, backgroundDest);
-DrawImage(player,origin,playerDest);
+Draw(player,background);
 
         EndDrawing(); 
     }
 
-    UnloadTexture(background);
-    UnloadTexture(player);
+   RemoveImageFromMem(player,background);
     CloseWindow();
     return 0;
+} 
+void CreateWindow(int width,int height,char *str){
+    InitWindow(width, height, str);
+    HideCursor();
+    SetTargetFPS(60);
+}
+void RemoveImageFromMem(Texture2D player,Texture2D background){
+    UnloadTexture(background);
+    UnloadTexture(player);
+}
+void Draw(Texture2D player,Texture2D background){
+    Vector2 origin = { 0, 0 }; 
+    Rectangle backgroundDest = { 0, 0, 800, 400 };
+    Rectangle playerDest = { 100, 100, 64, 64 };
+    DrawImage(background, origin, backgroundDest);
+    DrawImage(player,origin,playerDest);
 }
 void DrawImage(Texture2D sprite,Vector2 origin,Rectangle dest){
     DrawTexturePro(sprite, 
         (Rectangle){ 0, 0, (float)sprite.width, (float)sprite.height }, 
         (Rectangle)dest,                                         
         (Vector2)origin, 0.0f, WHITE);
+}
+void UpdatePlayer(Rectangle *playerDest){
+    Vector2 mousePos = GetMousePosition();
+    playerDest->x = mousePos.x - (playerDest->width / 2);
+playerDest->y = mousePos.y - (playerDest->height / 2);
 }
